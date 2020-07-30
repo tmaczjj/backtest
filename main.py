b957528c7b7ff002d45d67a7c0dda66baee90d54
zjj_tmac <524266571@qq.com>
2020-07-30 17:08:50 +0800

    add hist_order management

--- main.py
+++ main.py
@@ -14,9 +14,25 @@ pro = ts.pro_api('5fc1ae2a4708570262b751312b521760932f3170201a9842b28212cc')
 
 
 def run(trade_date):
+    ORDER_FILE_NAME = "order_hist_" + trade_date.strftime("%Y%m%d") + ".json"
+    ORDER_FILE_ROUTE = "reporter/order/" + ORDER_FILE_NAME
+    ACCOUNT_STAT_NAME = "stat_" + trade_date.strftime("%Y%m%d") + ".csv"
+    ACCOUNT_STAT_ROUTE = "reporter/account/" + ACCOUNT_STAT_NAME
     T0_broker = broker.T0BackTestBroker(cash=100000, deal_price="AskPrice1")
     mytest = MyBackTest(codelist, trade_date, broker=T0_broker)
     mytest.start()
+    order_lst = mytest.ctx.broker.order_hist_lst
+    with open(ORDER_FILE_ROUTE, "w") as wf:
+        json.dump(order_lst, wf, indent=4, default=str)
+    stats = mytest.stat
+    stats.data.to_csv(ACCOUNT_STAT_ROUTE)
+    # print("策略收益： {:.3f}%".format(stats.total_returns * 100))
+    # print("最大回彻率: {:.3f}% ".format(stats.max_dropdown * 100))
+    # print("年化收益: {:.3f}% ".format(stats.annual_return * 100))
+    # print("夏普比率: {:.3f} ".format(stats.sharpe))
+    #
+    # plotter = Plotter(stats, order_lst)
+    # plotter.report("reporter/report.png")
 
 
 if __name__ == '__main__':
@@ -38,17 +54,4 @@ if __name__ == '__main__':
     time_spent = time_end - time_start
     print("\n回测耗时--: {time}".format(time=time_spent))
 
-    # order_lst = mytest.ctx.broker.order_hist_lst
-    # with open("reporter/order_hist.json", "w") as wf:
-    #     json.dump(order_lst, wf, indent=4, default=str)
-    # stats = mytest.stat
-    # stats.data.to_csv("reporter/stat.csv")
-    # print("策略收益： {:.3f}%".format(stats.total_returns * 100))
-    # print("最大回彻率: {:.3f}% ".format(stats.max_dropdown * 100))
-    # print("年化收益: {:.3f}% ".format(stats.annual_return * 100))
-    # print("夏普比率: {:.3f} ".format(stats.sharpe))
-    #
-    # plotter = Plotter(stats, order_lst)
-    # plotter.report("reporter/report.png")
 
-    #
